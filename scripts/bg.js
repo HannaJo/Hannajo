d1bd34a092b81287d863039d47a960c7743b2e70
Jeremi Stadler <stadler.jeremi@gmail.com>
2017-05-15 14:04:19 +0200

    Add fixed topbar

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,6 +1,11 @@
 
 let lastBgOpacity = 1;
+let lastTop = 0;
 const maxVal = 200;
+
+function clamp(val, min, max) {
+  return Math.min(max, Math.max(min, val));
+}
 
 function onUpdate() {
   requestAnimationFrame(onUpdate);
@@ -9,9 +14,28 @@ function onUpdate() {
   const newVal = 1 - (Math.max(0, Math.min(maxVal, top)) / maxVal);
   if (newVal !== lastBgOpacity)
   {
-    lastBgOpacity = newVal;
     const bgElm = document.getElementById('bg');
     bgElm.style.opacity = newVal;
+
+    const tipping = 200;
+    const shouldToggleOnTopbar = top >= tipping && lastTop < tipping;
+    const shouldToggleOffTopbar = top < tipping && lastTop >= tipping;
+
+    if (shouldToggleOnTopbar) {
+      console.log('toggling on');
+      const topbarElm = document.getElementById('topbar');
+      topbarElm.classList.add('visible');
+    }
+
+    if (shouldToggleOffTopbar) {
+      console.log('toggling off');
+
+      const topbarElm = document.getElementById('topbar');
+      topbarElm.classList.remove('visible');
+    }
+
+    lastBgOpacity = newVal;
+    lastTop = top;
   }
 }
 
