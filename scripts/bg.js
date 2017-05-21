79ba2a40e91ba32725387c2320334058dd19021b
Jeremi Stadler <stadler.jeremi@gmail.com>
2017-05-21 22:13:10 +0200

    Complete topbar!

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,42 +1,69 @@
 
-let lastBgOpacity = 1;
-let lastTop = 0;
-const maxVal = 200;
+let lastTopPosition = -1;
+const ANIMATION = 200;
 
 function clamp(val, min, max) {
   return Math.min(max, Math.max(min, val));
 }
 
+function lerp(v1, v2, amount) {
+  return v1 + (v2 - v1) * clamp(amount, 0, 1);
+}
+
+
 function onUpdate() {
   requestAnimationFrame(onUpdate);
 
-  const top = window.pageYOffset || document.documentElement.scrollTop;
-  const newVal = 1 - (Math.max(0, Math.min(maxVal, top)) / maxVal);
-  if (newVal !== lastBgOpacity)
+  const topPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (topPosition !== lastTopPosition)
   {
+    lastTopPosition = topPosition;
+
     const bgElm = document.getElementById('bg');
-    bgElm.style.opacity = newVal;
+    bgElm.style.opacity = 1 - clamp(topPosition, 0, 200) / 200;
 
-    const tipping = 200;
-    const shouldToggleOnTopbar = top >= tipping && lastTop < tipping;
-    const shouldToggleOffTopbar = top < tipping && lastTop >= tipping;
+    const topbarElm = document.getElementById('topbar');
+    topbarElm.style.opacity = clamp(topPosition - 200, 0, 200) / 200;
 
-    if (shouldToggleOnTopbar) {
-      console.log('toggling on');
-      const topbarElm = document.getElementById('topbar');
-      topbarElm.classList.add('visible');
-    }
+    const logoElm = document.getElementById('topbar-logo');
+    const nameElm = document.getElementById('topbar-name');
 
-    if (shouldToggleOffTopbar) {
-      console.log('toggling off');
+    const windowWidth = window.innerWidth;
 
-      const topbarElm = document.getElementById('topbar');
-      topbarElm.classList.remove('visible');
-    }
+    const HEADER_HEIGHT = 350;
+    const logoAnim = clamp(topPosition, 0, HEADER_HEIGHT) / HEADER_HEIGHT;
+    const textAnim = clamp(topPosition, 0, HEADER_HEIGHT) / HEADER_HEIGHT;
+    const textColorAnim = clamp(topPosition - 150, 0, 200) / 200;
 
-    lastBgOpacity = newVal;
-    lastTop = top;
+
+    const bigLogoSize = clamp(windowWidth / 2, 0, 225);
+    const smallLogoSize = 40;
+    const logoSize = lerp(bigLogoSize, smallLogoSize, logoAnim);
+    logoElm.style.width = logoSize + 'px';
+    logoElm.style.height = logoSize + 'px';
+
+    const bigLogoLeft = windowWidth / 2 - logoSize / 2;
+    const smallLogoLeft = 16;
+    logoElm.style.left = lerp(bigLogoLeft, smallLogoLeft, logoAnim) + 'px';
+    logoElm.style.top = lerp(60, 5, logoAnim) + 'px';
+
+
+    const bigTextSize = clamp(windowWidth * 0.08, 14, 40);
+    const smallTextSize = 14;
+    const textSize = lerp(bigTextSize, smallTextSize, textAnim);
+
+    nameElm.style.fontSize = textSize + 'px';
+    nameElm.style.top = lerp(100 + logoSize, 1, textAnim) + 'px';
+    const nameWidth2 = nameElm.clientWidth / 2;
+    nameElm.style.right = lerp(windowWidth / 2 - nameWidth2, 16, textAnim) + 'px';
+
+    const lightness = Math.floor(lerp(255, 50, textColorAnim));
+    nameElm.style.color = `rgb(${lightness},${lightness},${lightness})`;
   }
 }
 
 requestAnimationFrame(onUpdate);
+window.onresize = function() {
+  lastTopPosition = -1;
+};
